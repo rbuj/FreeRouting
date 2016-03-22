5262095bbf5e366fc265dbc761ae0e577d5ba8b9
Robert Antoni Buj Gelonch <rbuj@fedoraproject.org>
2016-03-22 13:09:52 +0100

    refactor

---
+++ src/freeroute/gui/WindowPackages.java
@@ -0,0 +1,91 @@
+/*
+ *  Copyright (C) 2014  Alfons Wirtz  
+ *   website www.freerouting.net
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License at <http://www.gnu.org/licenses/> 
+ *   for more details.
+ *
+ * PackagesWindow.java
+ *
+ * Created on 7. Maerz 2005, 09:14
+ */
+
+package gui;
+
+import java.util.Iterator;
+import java.util.List;
+import library.Package;
+import library.Packages;
+
+/**
+ * Window displaying the library packagess.
+ *
+ * @author Alfons Wirtz
+ */
+public class WindowPackages extends WindowObjectListWithFilter
+{
+    
+    /** Creates a new instance of PackagesWindow */
+    public WindowPackages(BoardFrame p_board_frame)
+    {
+        super(p_board_frame);
+        java.util.ResourceBundle resources = 
+                java.util.ResourceBundle.getBundle("gui.resources.Default", p_board_frame.get_locale());
+        this.setTitle(resources.getString("packages"));
+        p_board_frame.set_context_sensitive_help(this, "WindowObjectList_LibraryPackages");
+    }
+    
+    /**
+     * Fills the list with the library packages.
+     */
+    @Override
+    protected void fill_list()
+    {
+        Packages packages = this.board_frame.board_panel.board_handling.get_routing_board().library.packages;
+        Package[] sorted_arr = new Package[packages.count()];
+        for (int i = 0; i < sorted_arr.length; ++i)
+        {
+            sorted_arr[i] = packages.get(i + 1);
+        }
+        java.util.Arrays.sort(sorted_arr);
+        for (int i = 0; i < sorted_arr.length; ++i)
+        {
+            this.add_to_list(sorted_arr[i]);
+        }
+        this.list.setVisibleRowCount(Math.min(packages.count(), DEFAULT_TABLE_SIZE));
+    }
+    
+    @Override
+    protected void select_instances()
+    {
+        List<Object> selected_packages = list.getSelectedValuesList();
+        if (selected_packages.isEmpty())
+        {
+            return;
+        }
+        board.RoutingBoard routing_board = board_frame.board_panel.board_handling.get_routing_board();
+        java.util.Set<board.Item> board_instances = new java.util.TreeSet<>();
+        java.util.Collection<board.Item> board_items = routing_board.get_items();
+        for (board.Item curr_item : board_items)
+        {
+            if (curr_item.get_component_no() > 0)
+            {
+                board.Component curr_component = routing_board.components.get(curr_item.get_component_no());
+                if (selected_packages.contains(curr_component.get_package()))
+                {
+                    board_instances.add(curr_item);
+                }
+            }
+        }
+        board_frame.board_panel.board_handling.select_items(board_instances);
+        board_frame.board_panel.board_handling.zoom_selection();
+    }
+}
