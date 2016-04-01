a79c131dd65162a0392918d50b9af8e2ffe31b0a
Robert Antoni Buj Gelonch <rbuj@fedoraproject.org>
2016-04-01 10:55:36 +0200

    Add author info in WindowAboutController.java

--- src/main/java/net/freerouting/freeroute/WindowAboutController.java
+++ src/main/java/net/freerouting/freeroute/WindowAboutController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Your Organisation
+ * Copyright (C) 2016 Robert Antoni Buj Gelonch <rbuj@fedoraproject.org>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
