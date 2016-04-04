1efc179292b45e309e9272b4680606b304ffa19b
Robert Antoni Buj Gelonch <rbuj@fedoraproject.org>
2016-04-04 23:55:32 +0200

    package-info.java: license is a comment and should be GNU GPL 3

--- src/main/java/net/freerouting/freeroute/package-info.java
+++ src/main/java/net/freerouting/freeroute/package-info.java
@@ -1,16 +1,18 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 /**
  * Contains  functionality of the graphical user interface  based on javax.swing.
- *
- * <p>{@literal @}({@literal #})package.html 1.60 98/01/27</p>
- *
- * <p>Copyright 1993-1997 Sun Microsystems, Inc. 901 San Antonio Road, Palo Alto,
- * California, 94303, U.S.A. All Rights Reserved.</p>
- *
- * <p>This software is the confidential and proprietary information of Sun
- * Microsystems, Inc. ("Confidential Information"). You shall not disclose such
- * Confidential Information and shall use it only in accordance with the terms
- * of the license agreement you entered into with Sun.</p>
- *
- * <p>CopyrightVersion 1.2</p>
  */
 package net.freerouting.freeroute;
