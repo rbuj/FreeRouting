b066f1b619887f9586668ae1c7e1117159b394c0
Robert Antoni Buj Gelonch <rbuj@fedoraproject.org>
2016-04-04 18:51:42 +0200

    maven: generate javahelp from docbook

---
+++ src/main/java/net/freerouting/freeroute/designformats/specctra/package-info.java
@@ -0,0 +1,18 @@
+/**
+ * Contains classes for reading and writing board designs in the Specctra dsn text format.
+ *
+ *
+ * @(#)package.html 1.60 98/01/27
+ * 
+ * Copyright 1993-1997 Sun Microsystems, Inc. 901 San Antonio Road, 
+ * Palo Alto, California, 94303, U.S.A.  All Rights Reserved.
+ * 
+ * This software is the confidential and proprietary information of Sun
+ * Microsystems, Inc. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Sun.
+ * 
+ * CopyrightVersion 1.2
+ */
+package net.freerouting.freeroute.designformats.specctra;
