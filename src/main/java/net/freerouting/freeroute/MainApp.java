ba4f4f686e1a163b3a540dc48e48b7912decc5bc
Robert Antoni Buj Gelonch <robert.buj@gmail.com>
2016-12-22 23:22:11 +0100

    Reliance on default encoding

--- src/main/java/net/freerouting/freeroute/MainApp.java
+++ src/main/java/net/freerouting/freeroute/MainApp.java
@@ -25,6 +25,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
@@ -130,7 +131,7 @@ public class MainApp extends Application {
                 // the locale is provided
                     case "-l":
                         String new_locale = args[i + 1].substring(0, 2);
-                        try (InputStream in = MainApp.class.getClass().getResourceAsStream("/LOCALES"); BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
+                        try (InputStream in = MainApp.class.getClass().getResourceAsStream("/LOCALES"); BufferedReader reader = new BufferedReader(new InputStreamReader(in,StandardCharsets.UTF_8))) {
                             String line = reader.readLine();
                             while (line != null) {
                                 if (line.equals(new_locale)) {
