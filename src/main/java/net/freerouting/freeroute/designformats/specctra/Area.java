8e93c1495c2cf3ec9a6f5c6ae3628f6429096f52
Robert Antoni Buj Gelonch <robert.buj@gmail.com>
2017-05-11 13:45:02 +0200

    Area, AreaReadable & AreaTransformable

---
+++ src/main/java/net/freerouting/freeroute/designformats/specctra/Area.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2017 Robert Antoni Buj Gelonch {@literal <}rbuj{@literal @}fedoraproject.org{@literal >}
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.freerouting.freeroute.designformats.specctra;
+
+import java.util.Collection;
+
+/**
+ *
+ * @author robert
+ */
+public class Area implements AreaReadable, AreaTransformable {
+
+    String area_name; // may be generated later on, if area_name is null.
+    final Collection<Shape> shape_list;
+    final String clearance_class_name;
+
+    public Area(String p_area_name, Collection<Shape> p_shape_list, String p_clearance_class_name) {
+        area_name = p_area_name;
+        shape_list = p_shape_list;
+        clearance_class_name = p_clearance_class_name;
+    }
+}
---
+++ src/main/java/net/freerouting/freeroute/designformats/specctra/AreaReadable.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright (C) 2017 Robert Antoni Buj Gelonch {@literal <}rbuj{@literal @}fedoraproject.org{@literal >}
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.freerouting.freeroute.designformats.specctra;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import static net.freerouting.freeroute.designformats.specctra.ShapeReadable.read_scope;
+
+/**
+ *
+ * @author robert
+ */
+public interface AreaReadable {
+    /**
+     * Reads a shape , which may contain holes from a specctra dsn-file. The
+     * first shape in the shape_list of the result is the border of the area.
+     * The other shapes in the shape_list are holes (windows).
+     */
+    static Area read_area_scope(Scanner p_scanner,
+            LayerStructure p_layer_structure, boolean p_skip_window_scopes) throws DsnFileException {
+        Collection<Shape> shape_list = new LinkedList<>();
+        String clearance_class_name = null;
+        String area_name = null;
+        boolean result_ok = true;
+        Object next_token;
+        try {
+            next_token = p_scanner.next_token();
+        } catch (java.io.IOException e) {
+            System.out.println("Shape.read_area_scope: IO error scanning file");
+            return null;
+        }
+        if (next_token instanceof String) {
+            String curr_name = (String) next_token;
+            if (!curr_name.isEmpty()) {
+                area_name = curr_name;
+            }
+        }
+        Shape curr_shape = read_scope(p_scanner, p_layer_structure);
+        if (curr_shape == null) {
+            result_ok = false;
+        }
+        shape_list.add(curr_shape);
+        next_token = null;
+        for (;;) {
+            Object prev_token = next_token;
+            try {
+                next_token = p_scanner.next_token();
+            } catch (java.io.IOException e) {
+                System.out.println("Shape.read_area_scope: IO error scanning file");
+                return null;
+            }
+            if (next_token == null) {
+                System.out.println("Shape.read_area_scope: unexpected end of file");
+                return null;
+            }
+            if (next_token == Keyword.CLOSED_BRACKET) {
+                // end of scope
+                break;
+            }
+
+            if (prev_token == Keyword.OPEN_BRACKET) {
+                // a new scope is expected
+                if (next_token == Keyword.WINDOW && !p_skip_window_scopes) {
+                    Shape hole_shape = read_scope(p_scanner, p_layer_structure);
+                    shape_list.add(hole_shape);
+                    // overread closing bracket
+                    try {
+                        next_token = p_scanner.next_token();
+                    } catch (java.io.IOException e) {
+                        System.out.println("Shape.read_area_scope: IO error scanning file");
+                        return null;
+                    }
+                    if (next_token != Keyword.CLOSED_BRACKET) {
+                        System.out.println("Shape.read_area_scope: closed bracket expected");
+                        return null;
+                    }
+
+                } else if (next_token == Keyword.CLEARANCE_CLASS) {
+                    clearance_class_name = DsnFile.read_string_scope(p_scanner);
+                } else {
+                    // skip unknown scope
+                    ScopeKeyword.skip_scope(p_scanner);
+                }
+            }
+        }
+        if (!result_ok) {
+            return null;
+        }
+        return new Area(area_name, shape_list, clearance_class_name);
+    }
+}
