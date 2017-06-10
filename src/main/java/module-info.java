7bedf6022b90391d47527a1c035dabfea5a78cd4
Robert Antoni Buj Gelonch <robert.buj@gmail.com>
2017-06-10 02:02:49 +0200

    add module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2017 Robert Antoni Buj Gelonch {@literal <}rbuj{@literal @}fedoraproject.org{@literal >}
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+module Freeroute {
+    requires java.desktop;
+    requires java.logging;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.swing;
+    requires junit;
+    requires hamcrest.core;
+    requires javahelp;
+    requires guava;
+}
